42a3f48d40462d0131c1bc11bd27e4f1663ca980
lk543061231 <543061231@qq.com>
2023-09-14 21:57:04 +0800

    12313

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,16 +1,42 @@
 <template>
-	<view >
-		<navbar  @onRightClick="onRightClick" :mysNavConfig="mysNavConfig">
-			<view class="s-wrap row" slot="center1" @click="toSearch">
-				<image src="@/static/images/search.png" class="icon-img"></image>
-				<text>请输入</text>
-			</view>
-			<view class="search" slot="right">
-				搜索
-			</view>
-		</navbar>
+	<view class="home-wrap">
 		<view class="index-top">
+			<view class="nav-wrap"  >
+				<view class="s-wrap row" @click="toSearch">
+					<image src="@/static/images/search.png" class="icon-img"></image>
+					<text>请输入</text>
+				</view>
+				<view class="search" slot="right">
+					搜索
+				</view>
+			</view>
 			<view class="index-bg">
+				<u-swiper
+				    :list="list1"
+					circular
+					indicator
+					:autoplay="false"
+					height="150"
+				>
+				</u-swiper>
+			</view>
+			<view class="top_label row jc-sb">
+				<view class="label"> 品质保障 </view>
+				<view class="label"> 售后无忧 </view>
+				<view class="label"> 甄选好惠 </view>
+				<view class="label"> 能省能赚 </view>
+			</view>
+			
+		</view>
+		<view class="cate-wrap ">
+			<view class="row cate-list">
+				<view class="cate-item column" v-for="(item,index) in 8" :key="index">
+					<image src="@/static/img1.png" class="c-img"></image>
+					<text class="c-label">123</text>
+				</view>
+			</view>
+			
+			<view class="cate-swiper">
 				<u-swiper
 				    :list="list1"
 					circular
@@ -22,40 +48,74 @@
 			</view>
 			
 		</view>
-		<view class="cate-wrap ">
-			<view class="title row jc-sb">
-				<view class="left row">
-					<image class="f-img" src="@/static/images/home-fl.png" mode=""></image>
-					<text click="l-txt">车型分类</text>
-				</view>
-				<view class="right row"  @click="navTo('/pagesA/carType/carType')">
-					<text click="r-txt">更多</text>
-					<u-icon name="arrow-right" size="13"></u-icon>
-				</view>
+		<view class="buying-wrap">
+			<view class="title">
+				限时抢购
 			</view>
-			<view class="model-wrap mt20">
-				<swiper class="swiper" :indicator-dots="true" :autoplay="false"  indicator-active-color="#FF8D51">
-					<swiper-item v-for="(item,index) in 3" class="row jc-sb swiper-item" :key="index" >
-						<view class="swiper-item column" v-for="(e,i) in 4" :key="i">
-							<image src="@/static/images/car2.png" class="c-img"></image>
-							<text class="c-name">分类名称</text>
+			<scroll-view class="" scroll-x="true" >
+				<view class="buying-list">
+					<view class="buying-item" v-for="(item,index) in 6" :key="index">
+						<image src="@/static/home1.png" class="b-img"></image>
+						<view class="name twoHidden">名称名称名称名称名称名称名称名称名称名称名称名称名称名称名称</view>
+						<view class="price-wrap">
+							<text class="p1">￥12</text>
+							<text class="p2">￥12</text>
 						</view>
-					</swiper-item>
-				</swiper>
+						<view class="time">
+							
+						</view>
+						<view class="buy">
+							立即抢购
+						</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+		<view class="buying-after row jc-sa">
+			<view class="a-item">
+				往期抢购
+			</view>
+			<view class="a-item">
+				下期抢购
+			</view>
+			<view class="a-item">
+				预约使用
 			</view>
 		</view>
-		<view class="cate-wrap order ">
-			<view class="title row jc-sb">
-				<view class="left row">
-					<image class="f-img" src="@/static/images/home-dd.png" mode=""></image>
-					<text click="l-txt">最新订单</text>
-				</view>
-			</view>
-			<view class="order-list mt20">
-				
-			</view>
+		<view class="pd20">
+			<u-swiper
+			    :list="list1"
+				circular
+				indicator
+				:autoplay="false"
+				height="90"
+			>
+			</u-swiper>
 		</view>
 		
+		
+		<view class="con-box">
+			<view class="con-top row jc-sb">
+				<view class="title">
+					标题 
+				</view>
+				<view class="more">
+					查看更多
+				</view>
+			</view>
+			<view class="list">
+				<view class="item row jc-sb" v-for="(item,index) in 6" :key="index">
+					<image src="../../static/home1.png" class="c-img"></image>
+					<view class="info column jc-sb">
+						<view class="name twoHidden">名称名称名称名称名称名称名称名称名称名称名称名称名称名称名称名称名称名称</view>
+						<view class="price">
+							<text class="p1"> ￥123 </text>
+							<text class="p1"> ￥123 </text>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 		<tabbar :activeIndexProp="0"></tabbar>
 	</view>
 </template>
@@ -67,16 +127,15 @@
 				mysNavConfig:{
 					isHome:false,
 					navPadding:false,
-					navFixed:true,
-					bgColor:'#FB852D',
+					navFixed:false,
 					// searchConfig:{
 					// 	placeholder:'输入关键词搜索'
 					// }
 				},
 				list1:[
-					'http://asses.ytq.wiki/uploads/20221129/5dc95a3f153fefc865a1fb485536c24aqJ8pcKVwA0atGPHx.jpg',
-					'http://asses.ytq.wiki/uploads/20221129/5dc95a3f153fefc865a1fb485536c24aqJ8pcKVwA0atGPHx.jpg',
-					'http://asses.ytq.wiki/uploads/20221129/5dc95a3f153fefc865a1fb485536c24aqJ8pcKVwA0atGPHx.jpg',
+					require('@/static/home1.png'),
+					require('@/static/home1.png'),
+					require('@/static/home1.png'),
 				],
 				cateList:[
 					
@@ -112,75 +171,134 @@
 </script>
 
 <style lang="scss" scoped>
-.search{
-	color: #fff;
-}
-.s-wrap{
+.home-wrap{
+	min-height: 100vh;
 	background-color: #fff;
-	width: 81%;
-	padding:16upx 24upx;
-	border-radius: 32upx;
-	color: #7F7F7F;
-	font-size: 24upx;
-	.icon-img{
-		width: 28upx;
-		height: 28upx;
-		margin-right: 34upx;
-	}
+	padding-bottom: 130upx;
 }
-
 .index-top{
-	background: linear-gradient(0deg, #FB852D, #FF8968);
 	width: 100%;
-	height: 430upx;
-	border-radius: 0 0 35% 35%;
+	height: 530upx;
+	background-color: red;
+	background-image: url('@/static/bg2.png');
+	// padding-top: 100upx;
+	.nav-wrap{
+		padding: 20upx;
+	}
+	.search{
+		color: #fff;
+	}
+	.s-wrap{
+		padding:16upx 24upx;
+		border-radius: 32upx;
+		font-size: 24upx;
+		background-color: #fff;
+		.icon-img{
+			width: 28upx;
+			height: 28upx;
+			margin-right: 34upx;
+		}
+	}
 	.index-bg{
-		padding:200upx 30upx 0 30upx;
+		padding: 20upx;
+	}
+	.top_label{
+		padding: 0 20upx;
+		.label{
+			color: #fff;
+		}
 	}
 }
 .cate-wrap{
-	padding: 30upx;
-	margin-top: 80upx;
-	&.order{
-		margin-top: 0upx;
+	padding: 20upx;
+	background-color: #fff;
+	border-radius: 30upx;
+	margin-top: -30upx;
+	.cate-list{
+		flex-wrap: wrap;
+		justify-content: space-between;
 	}
-	.title{
-		.left{
-			.f-img{
-				width: 38upx;
-				height: 38upx;
-				margin-right: 12upx;
-			}
-			.l-txt{
-				color: #333333;
-				font-size: 32upx;
-				font-weight: bold;
-			}
-		}
-		.right{
-			color: #999999;
-			font-size: 26upx;
+	.cate-item{
+		margin-bottom: 20upx;
+		.c-img{
+			width: 150upx;
+			height: 150upx;
 		}
 	}
-	.swiper{
-		height: 200upx;
-		/deep/ swiper-item{
-			height: 75% !important;
-		}
-		.swiper-item{
-			.c-img{
-				width: 107upx;
-				height: 107upx;
-			}
-			.c-name{
-				color: #333333;
-				font-size: 26upx;
-			}
-		}
-	}
-	.order-list{
-		padding-bottom: 200upx;
+	.cate-swiper{
+		
 	}
 	
+}
+.buying-wrap{
+	padding: 20upx;
+	// background-image: url('@/static/bg2.png');
+	background-color:rgba(225, 22, 22, 0.5);
+	border-radius: 30upx;
+	.title{
+		color: #fff;
+		font-size: 40upx;
+		font-weight: 600;
+		padding-left: 20upx;
+		position: relative;
+		&::after{
+			display: block;
+			content: "";
+			position: absolute;
+			width:10upx;
+			height: 100%;
+			background-color: #fff;
+			left: 0;
+			top: 0;
+		}
+	}
+	.buying-list{
+		display: flex;
+		flex-wrap: nowrap;
+		width: 100%;
+		margin-top: 20upx;
+		.buying-item{
+			width: 260upx;
+			display: inline-block;
+			margin-right: 20upx;
+			background-color: #fff;
+			border-radius: 20upx;
+			&:last-child{
+				margin-right: 0;
+			}
+			.b-img{
+				width: 260upx;
+				height: 260upx;
+			}
+		}
+	}
+	
+}
+.pd20{
+		padding: 20upx;
+	}
+.con-box{
+	background-color: rgba(25, 222, 22, 0.5);
+	border-radius: 30upx;
+	padding: 30upx 20upx;
+	margin-top: 30upx;
+	.list{
+		margin-top: 30upx;
+		.item{
+			margin-bottom: 20upx;
+			border-radius: 20upx;
+			overflow: hidden;
+			height: 200upx;
+			.c-img{
+				flex-shrink: 0;
+				width: 400upx;
+				height: 100%;
+			}
+			.info{
+				height: 100%;
+				background-color: #fff;
+			}
+		}
+	}
 }
 </style>
